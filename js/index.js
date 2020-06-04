918399e932799544fa09c7afd5f213b60004191a
Hong Baojin <13809735212.163.com>
2020-06-04 18:31:12 +0800

    base finished

--- js/index.js
+++ js/index.js
@@ -1,37 +1,13 @@
-// 显示弹窗
-function gaodun(){
-  $('.gaodun-mask').show();
-};
-// 证书名称
-var ccieName = '';
-// 去尾
-var noTail = '';
-// 选择证书
-$('.ck li').click(function(){
-  $(this).toggleClass('on');
-  ccieName = '';
-  // 赋值标题
-  $('.ck li').each(function(){
-    if($(this).hasClass('on')){
-      ccieName += $(this).text() + '、';
-    }
-  });
-  // 去掉尾部逗号
-  noTail = ccieName.substr(0, ccieName.length - 1);
-  $('#ccieVal').text(noTail);
-});
-// 点击提交按钮
-$('#sb').click(function(){
-  ccieName = '';
-  $('.ck li').each(function(){
-    if($(this).hasClass('on')){
-      ccieName += $(this).text() + '、';
-    }
-  });
-  noTail = ccieName.substr(0, ccieName.length - 1);
-  $('#ccie').val(noTail);
-});
-// 隐藏弹窗
-$('.gaodun-mask .close').click(function(){
-  $('.gaodun-mask').hide();
+$(function(){
+  function  clickHandle(btn, cont, classname) {
+    $(btn).each(function (i) {
+      var that = this;
+      $(that).click(function () {
+        $(that).addClass(classname).siblings().removeClass(classname);
+        $($(cont)[i]).addClass(classname).siblings().removeClass(classname);
+      })
+    })
+  };
+
+  clickHandle('.head span', '.cont .c-item', 'on');
 });
